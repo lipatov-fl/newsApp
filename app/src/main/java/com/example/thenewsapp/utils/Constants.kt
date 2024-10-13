357583fe4f90622d54d4ead4706a8a6043ed7bb6
Aleksandr L. <lipatov-fl@yandex.ru>
2024-10-13 21:59:15 +0300

    added layout and connected retrofit

---
+++ app/src/main/java/com/example/thenewsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "37bc46e776dd4fd0a22bcc2745c93ec8"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
